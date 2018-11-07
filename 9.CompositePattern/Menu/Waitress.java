b4c2da212e1555b599214c6077f14b390300a0a3
swk3169 <swk3169@gmail.com>
2018-11-07 22:32:24 +0900

    CompositePattern
    +UML
    +Document
    +Menu

---
+++ 9.CompositePattern/Menu/Waitress.java
@@ -0,0 +1,16 @@
+package MenuCompositePattern;
+
+public class Waitress
+{
+	MenuComponent allMenus;
+	
+	public Waitress(MenuComponent allMenus)
+	{
+		this.allMenus = allMenus;
+	}
+	
+	public void printMenu()
+	{
+		allMenus.print();
+	}
+}
